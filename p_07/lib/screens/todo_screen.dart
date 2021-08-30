2e040893b2383048cc0e030da0634ab53ca6551a
sinaziaee <sinaziaee99@gmail.com>
2021-08-30 17:58:27 +0430

    added hive package to p_07

--- p_07/lib/screens/todo_screen.dart
+++ p_07/lib/screens/todo_screen.dart
@@ -14,20 +14,52 @@ class TodoScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('$type Todo'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: textEditingController,
-          ),
-          TextButton.icon(
-            onPressed: () {},
-            icon: Icon(
-              type.toLowerCase() == 'add' ? Icons.add : Icons.edit,
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
             ),
-            label: Text(type),
-          ),
-        ],
+            TextField(
+              controller: textEditingController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                fillColor: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              onPressed: () {
+                onButtonPressed();
+              },
+              icon: Icon(
+                type.toLowerCase() == 'add' ? Icons.add : Icons.edit,
+                color: Colors.black,
+              ),
+              label: Text(
+                type,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  onButtonPressed(){
+    String value = textEditingController.text;
+    print(value);
+  }
+
 }
